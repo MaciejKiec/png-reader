6f954437e887b94d22dd51898402152ab644b1a8
MaciejKiec <harmonicisland8@gmail.com>
2023-04-30 15:04:27 +0200

    pHYsChunk full implementation

--- inc/Chunks/AncillaryChunks/pHYsChunk.hh
+++ inc/Chunks/AncillaryChunks/pHYsChunk.hh
@@ -2,6 +2,11 @@
 #define pHYs_CHUNK_HH
 #include "../Chunk.hh"
 class pHYsChunk: public Chunk{
+    private:
+    unsigned int horizontalResolution;
+    unsigned int verticalResolution;
+    unsigned int unitSpecifier;
+    void readChunkSpecifics();
     public:
         pHYsChunk(uint32_t _dataLength, uint32_t _type, std::vector<unsigned int> _data, uint32_t _crc32);
         void whatChunkAmI() override;
--- src/Chunks/AncillaryChunks/pHYsChunk.cpp
+++ src/Chunks/AncillaryChunks/pHYsChunk.cpp
@@ -1,8 +1,23 @@
 #include "Chunks/AncillaryChunks/pHYsChunk.hh"
 
+void pHYsChunk::readChunkSpecifics(){
+    horizontalResolution = (this->data[0] << 24 | this->data[1] << 16 | this->data[2] << 8 | this->data[3]);
+    verticalResolution = (this->data[4] << 24 | this->data[5] << 16 | this->data[6] << 8 | this->data[7]);
+    unitSpecifier = this->data[8];
+}
+
 pHYsChunk::pHYsChunk(uint32_t _dataLength, uint32_t _type, std::vector<unsigned int> _data, uint32_t _crc32):
-Chunk(_dataLength, _type, _data, _crc32) {};
+Chunk(_dataLength, _type, _data, _crc32) {
+        readChunkSpecifics();
+};
 
 void pHYsChunk::whatChunkAmI(){
-    std::cout << "I'm pHYs Chunk! I'm being implemented right now!\n\n";
+    std::cout << "Horizontal resolution: " << horizontalResolution << " pixels\n";
+    std::cout << "Vertical resolution: " << verticalResolution << " pixels\n";
+    if(unitSpecifier){
+        std::cout << "Unit is the meter\n";
+    }else{
+        std::cout << "Unit is unknown\n";
+    }
+    std::cout << std::endl;
 }
