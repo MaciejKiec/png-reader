e61fc2044688232de31d39b3e28d60473d55bdde
MaciejKiec <harmonicisland8@gmail.com>
2023-04-30 14:49:55 +0200

    sBITChunk full implementation

--- inc/Chunks/AncillaryChunks/sBITChunk.hh
+++ inc/Chunks/AncillaryChunks/sBITChunk.hh
@@ -2,6 +2,12 @@
 #define sBIT_CHUNK_HH
 #include "../Chunk.hh"
 class sBITChunk: public Chunk{
+    private:
+        unsigned int redBits;
+        unsigned int greenBits;
+        unsigned int blueBits;
+        unsigned int alphaBits;
+        void readChunkSpecifics();
     public:
         sBITChunk(uint32_t _dataLength, uint32_t _type, std::vector<unsigned int> _data, uint32_t _crc32);
         void whatChunkAmI() override;
--- src/Chunks/AncillaryChunks/sBITChunk.cpp
+++ src/Chunks/AncillaryChunks/sBITChunk.cpp
@@ -1,8 +1,21 @@
 #include "Chunks/AncillaryChunks/sBITChunk.hh"
 
+void sBITChunk::readChunkSpecifics(){
+    redBits = this->data[0];
+    greenBits = this->data[1];
+    blueBits = this->data[2];
+    alphaBits = this->data[3];
+}
+
 sBITChunk::sBITChunk(uint32_t _dataLength, uint32_t _type, std::vector<unsigned int> _data, uint32_t _crc32):
-Chunk(_dataLength, _type, _data, _crc32) {};
+Chunk(_dataLength, _type, _data, _crc32) {
+    readChunkSpecifics();
+};
 
 void sBITChunk::whatChunkAmI(){
-    std::cout << "I'm sBIT Chunk! I'm being implemented right now!\n\n";
+    std::cout << "Red bits: " << redBits << "\n";
+    std::cout << "Green bits: " << greenBits << "\n";
+    std::cout << "Blue bits: " << blueBits << "\n";
+    std::cout << "Alpha bits: " << alphaBits << "\n";
+    std::cout << std::endl;
 }
