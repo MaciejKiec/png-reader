98707d45977bf2edd5aae6913ab5be3996df0b11
MaciejKiec <harmonicisland8@gmail.com>
2023-06-15 18:17:56 +0200

    Added typedefs to increase legibility

--- src/Chunks/CriticalChunks/IDATChunk.cpp
+++ src/Chunks/CriticalChunks/IDATChunk.cpp
@@ -1,7 +1,11 @@
 #include "Chunks/CriticalChunks/IDATChunk.hh"
+#include "RSA/ChunkDataAdapter.hh"
+#include <iostream>
 
 IDATChunk::IDATChunk(uint32_t _dataLength, uint32_t _type, std::vector<unsigned int> _data, uint32_t _crc32):
-    Chunk(_dataLength, _type, _data, _crc32) {};
+    Chunk(_dataLength, _type, _data, _crc32) {
+        std::cout << ChunkDataToString(_data);
+    };
 
 void IDATChunk::whatChunkAmI(){
     std::cout << "I'm IDAT chunk!\n";
--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,13 @@
 #include "ChunkWriter.hh"
 #include "RSA/keysGenerator.hh"
 #include "RSA/fileEncryptorDecryptor.hh"
+typedef std::vector<unsigned int> PngFile;
+typedef const std::vector<Chunk*> Chunks;
 
 int main(){
 
-    generateRSAKeyPair("keys/public.txt", "keys/private.txt", 2048);
-    const std::string publicKeyPath = "keys/public.txt";
-    const std::string privateKeyPath = "keys/private.txt";
-    
-    std::string data = "Pewne dane";
-    
-    std::string encrypted = encryptRSA(data, publicKeyPath);
-    std::cout << std::endl<< encrypted << std::endl;
-
-    std::string decrypted = decryptRSA(encrypted, privateKeyPath);
-
-    std::cout << std::endl << decrypted << std::endl;
+    PngFile fileByes = loadBytesFromFile("images/1.png");
+    ChunkReader chunkReader(fileByes);
+    Chunks chunks = chunkReader.readSomeChunks();clear
     return 0;
 }
